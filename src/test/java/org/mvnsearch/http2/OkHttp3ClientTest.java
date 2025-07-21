057912d06e0618f2499d8e2c580a6182b3df7d1e
Libing Chen <libing.chen@gmail.com>
2025-07-21 14:36:35 -0700

    chore: polishing

--- src/test/java/org/mvnsearch/http2/OkHttp3ClientTest.java
+++ src/test/java/org/mvnsearch/http2/OkHttp3ClientTest.java
@@ -13,44 +13,44 @@ import java.security.KeyStore;
 import java.util.Arrays;
 
 public class OkHttp3ClientTest {
-    private static OkHttpClient client;
+  private static OkHttpClient client;
 
-    @BeforeAll
-    public static void setup() throws Exception {
-        File keyStoreFile = new File(System.getProperty("user.home") + "/data/certs/localhost.p12");
-        KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        keyStore.load(new FileInputStream(keyStoreFile), "changeit".toCharArray());
+  @BeforeAll
+  public static void setup() throws Exception {
+    File keyStoreFile = new File(System.getProperty("user.home") + "/data/certs/localhost.p12");
+    KeyStore keyStore = KeyStore.getInstance("PKCS12");
+    keyStore.load(new FileInputStream(keyStoreFile), "changeit".toCharArray());
 
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
-                TrustManagerFactory.getDefaultAlgorithm());
-        trustManagerFactory.init(keyStore);
-        TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-        if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-            throw new IllegalStateException("Unexpected default trust managers:"
-                    + Arrays.toString(trustManagers));
-        }
-        X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
-
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, new TrustManager[]{trustManager}, null);
-        SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
-
-        client = new OkHttpClient.Builder()
-                .sslSocketFactory(sslSocketFactory, trustManager)
-                .hostnameVerifier((s, sslSession) -> true)
-                .build();
+    TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+      TrustManagerFactory.getDefaultAlgorithm());
+    trustManagerFactory.init(keyStore);
+    TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+    if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+      throw new IllegalStateException("Unexpected default trust managers:"
+        + Arrays.toString(trustManagers));
     }
+    X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
+
+    SSLContext sslContext = SSLContext.getInstance("TLS");
+    sslContext.init(null, new TrustManager[]{trustManager}, null);
+    SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+    client = new OkHttpClient.Builder()
+      .sslSocketFactory(sslSocketFactory, trustManager)
+      .hostnameVerifier((s, sslSession) -> true)
+      .build();
+  }
 
 
-    @Test
-    public void testHelloEndpoint() throws Exception {
-        Request request = new Request.Builder()
-                .url("https://localhost:8443/")
-                .build();
+  @Test
+  public void testHelloEndpoint() throws Exception {
+    Request request = new Request.Builder()
+      .url("https://localhost:8443/")
+      .build();
 
-        try (Response response = client.newCall(request).execute()) {
-            System.out.println(response.body().string());
-        }
+    try (Response response = client.newCall(request).execute()) {
+      System.out.println(response.body().string());
     }
+  }
 
 }
